a63b2b0e0f892d1cab8535b1596e86f77ceeff82
yunshi <yunshi@wacai.com>
2015-10-12 18:13:44 +0800

    setup skeleton

---
+++ docs/tech.md
@@ -0,0 +1,2 @@
+1. <https://github.com/ulisesbocchio/jasypt-spring-boot>
+
---
+++ src/main/java/com/keevol/BookstoryApplication.java
@@ -0,0 +1,12 @@
+package com.keevol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoryApplication.class, args);
+    }
+}
